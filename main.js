8c925f6974d91baad1fdb4fca7f016cea3da8eea
minidiscs <shanedbutler@gmail.com>
2022-04-30 21:03:35 -0400

    rework fn

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 function game(){
     for (let round = 0; round < 5; round++) {
-        const playerInput = prompt("What's your move?");
-        const computerInput = computerPlay();
-        playRound(playerInput, computerInput);
-        console.log(playerInput + ' vs ' + computerInput);
-        console.log(playRound(playerInput, computerInput));
+        let playerInput = prompt("What's your move?");
+        playerInput = playerInput.toLocaleLowerCase();
+        let computerInput = computerPlay();
+        computerInput = computerInput.toLocaleLowerCase();
+        let winner = calcRoundWinner(playerInput, computerInput);
+        console.log(`It's ${playerInput} vs ${computerInput}:`);
+        if (winner == 'player'){
+            console.log(`Player wins, player's ${playerInput} beats computer's ${computerInput}`)
+        }
+        else if (winner == 'computer'){
+            console.log(`Computer wins, player's ${playerInput} does not beat computer's ${computerInput}`)
+        }
+        else if (winner == 'none') {
+            round -= 1;
+            console.log(`Draw, player's ${playerInput} equals computer's ${computerInput}. Play round again.`)
+        }
+        else {
+            round -= 1;
+            console.log("There has been an error. The round has not been counted.");
+        }
     }
 }
 
@@ -13,49 +28,31 @@ function computerPlay(){
     return hands[ Math.floor(Math.random() * hands.length)];
 }
 
-function playRound(playerPlay, computerPlay){
-    let playerMove = playerPlay.toLowerCase();
-    let computerMove = computerPlay.toLowerCase();
+function calcRoundWinner(playerPlay, computerPlay){
+    
     if (playerPlay === computerPlay){
-        let winner = 0;
-        return result = 'Draw';
+        return 'none';
     }
-    else if (computerMove == 'paper' && computerMove == 'rock'){
-        let winner = 'player';
-        return result = 'Paper beats rock, player wins!';
+    else if (playerPlay == 'paper' && computerPlay == 'rock'){
+        return 'player';
     }
-    else if (playerMove == 'scissors' && computerMove == 'rock'){
-        let winner = 'computer';
-        return result = 'Rock beats scissors, computer wins!';
+    else if (playerPlay == 'scissors' && computerPlay == 'rock'){
+        return 'computer';
     }
-    else if (playerMove == 'rock' && computerMove == 'paper'){
-        let winner = 'player';
-        return result = 'Rock beats paper, player wins!';
+    else if (playerPlay == 'rock' && computerPlay == 'paper'){
+        return 'player';
     }
-    else if (playerMove == 'scissors' && computerMove == 'paper'){
-        let winner = 'player';
-        return result = 'Scissors beats paper, player wins!';
+    else if (playerPlay == 'scissors' && computerPlay == 'paper'){
+        return 'player';
     }
-    else if (playerMove == 'paper' && computerMove == 'scissors'){
-        let winner = 'computer';
-        return result = 'Scissors beats paper, computer wins!';
+    else if (playerPlay == 'paper' && computerPlay == 'scissors'){
+        return 'computer';
     }
-    else if (playerMove == 'rock' && computerMove == 'scissors'){
-        let winner = 'computer';
-        return result = 'Scissors beats paper, computer wins!';
-    }
-    else{
-        return result ='Please input a valid move!'
-    }
-
-    if (winner == 'player'){
-        console.log("Player wins, player's ${playerMove} beats computer's ${computerMove}")
-    }
-    else if (winner == 'computer'){
-        console.log("Computer wins, player's ${playerMove} does not beat computer's ${computerMove}")
+    else if (playerPlay == 'rock' && computerPlay == 'scissors'){
+        return 'computer';
     }
     else {
-        console.log("Draw, player's ${playerMove} equals computer's ${computerMove}")
+        return 'error';
     }
 }
 
