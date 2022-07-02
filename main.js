7c715d922cc5f2872acf5bc521e143087abc511c
minidiscs <shanedbutler@gmail.com>
2022-07-02 16:53:23 -0400

    refactor code

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-function game(){
-    for (let round = 0; round < 5; round++) {
-        const playerInput = prompt("What's your move?");
+function game(playerInput){
+
         const computerInput = computerPlay();
-        playRound(playerInput, computerInput);
-        console.log(playerInput + ' vs ' + computerInput);
-        console.log(playRound(playerInput, computerInput));
-    }
+        let result = playRound(playerInput, computerInput);
+        if (result === 'draw') {
+            return 'Draw'
+        }
+        else if (result === 'player') {
+            return `Player's ${playerInput} beats ${computerInput}, player wins!`;
+        }
+        else if (result === 'computer') {
+            return `Player's ${playerInput} fails to beat ${computerInput}, computer wins!`;
+        }
 }
 
 function computerPlay(){
@@ -17,29 +22,63 @@ function playRound(playerPlay, computerPlay){
     let playerMove = playerPlay.toLowerCase();
     let computerMove = computerPlay.toLowerCase();
     if (playerPlay === computerPlay){
-        return result = 'Draw'
+        return 'draw';
     }
-    else if (computerMove == 'paper' && computerMove == 'rock'){
-        return result = 'Paper beats rock, player wins!';
+    else if (
+        playerMove === 'paper' && computerMove === 'rock' || 
+        playerMove === 'rock' && computerMove === 'scissors' || 
+        playerMove === 'scissors' && computerMove === 'paper' 
+        ){
+            return 'player';
     }
-    else if (playerMove == 'scissors' && computerMove == 'rock'){
-        return result = 'Rock beats scissors, computer wins!';
-    }
-    else if (playerMove == 'rock' && computerMove == 'paper'){
-        return result = 'Rock beats paper, player wins!';
-    }
-    else if (playerMove == 'scissors' && computerMove == 'paper'){
-        return result = 'Scissors beats paper, player wins!';
-    }
-    else if (playerMove == 'paper' && computerMove == 'scissors'){
-        return result = 'Scissors beats paper, computer wins!';
-    }
-    else if (playerMove == 'rock' && computerMove == 'scissors'){
-        return result = 'Scissors beats paper, computer wins!';
+    else if (
+        playerMove === 'paper' && computerMove === 'scissors' || 
+        playerMove === 'rock' && computerMove === 'paper' ||
+        playerMove === 'scissors' && computerMove === 'rock'
+        ){
+            return 'computer';
     }
     else{
-        return result ='Please input a valid move!'
+        return 'Please input a valid move!';
     }
 }
 
-game();
+const body = document.body;
+
+/* const buttonRock = document.createElement('button');
+body.append(buttonRock);
+buttonRock.innerText = "Rock"
+
+const buttonPaper = document.createElement('button');
+body.append(buttonPaper);
+buttonPaper.innerText = "Paper"
+
+const buttonScissors = document.createElement('button');
+body.append(buttonScissors);
+buttonScissors.innerText = "Scissors"
+
+or */
+let gameResult =  ''
+
+const btnR = document.querySelector('#rock');
+btnR.addEventListener('click', () => {
+  gameResult = game("Rock");
+  console.log(gameResult);
+});
+
+const btnP = document.querySelector('#paper');
+btnP.addEventListener('click', () => {
+    gameResult = game("Paper");
+    console.log(gameResult);
+});
+
+const btnS = document.querySelector('#scissors');
+btnS.addEventListener('click', () => {
+    gameResult = game("Scissors");
+    console.log(gameResult);
+});
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+  alert("Hello World");
+});
